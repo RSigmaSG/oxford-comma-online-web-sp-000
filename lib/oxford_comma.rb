f0c7d68272707d053d29062939589ebaf3361a03
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-15 03:12:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,10 @@ def oxford_comma(array)
   out_string = ""
   array.each_with_index do |element, index| 
     
-    if (array.length == 1 || (array.length == 2 && index  == 0))
-      out_string << "#{element} "
+    if (index == 0)
+      out_string << "#{element}"
     elsif (index != (array.length - 1))
-      out_string << "#{element}, "
+      out_string << ", #{element},"
     elsif (index == (array.length - 1))
       out_string << "and #{element}";
     end
