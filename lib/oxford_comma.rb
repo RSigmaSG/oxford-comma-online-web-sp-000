59acfc632cb9b7d8f87268016c5a598e6f9ff09b
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-15 03:06:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,14 @@
 def oxford_comma(array)
   
   out_string = ""
-  array.each_with_index do |element, index| {
+  array.each_with_index do |element, index| 
+    
     if (index != (array.length - 1))
       out_string.push "#{element}, "
     else if (index == (array.length - 1))
       #out_string << "and #{element}";
     end
-  }
+  
   end
   
   puts out_string
