04edbbf0cbdd1e451297c82857622113c309076c
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-15 03:02:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
   out_string = ""
   array.each_with_index do |element, index| {
     if (index < (array.length - 1))
-      outstring << "#{element}, "
+      outstring << "#{element}, ";
     else if (index == array.length - 1)
-      outstring << "and #{element}"
+      outstring << "and #{element}";
     end
   }
   end
